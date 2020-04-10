c801b5ae1027abfcebd1a1049bb6f0dcd8398dc0
Guillaume Wells <9883518+Bulliby@users.noreply.github.com>
2020-04-10 09:34:28 +0200

    Feature : custom HTML on menu items

--- resources/views/front/partials/tree-item.blade.php
+++ resources/views/front/partials/tree-item.blade.php
@@ -1,7 +1,12 @@
 <li data-depth="{{ $tree['infos']['depth'] }}" class="{{ !empty($tree['children']) ? 'has-sub-menu' : '' }}">
-    <a href="{{ $tree['infos']['href'] }}" class="{{ $tree['infos']['htmlClasses'] }}" {{ $tree['infos']['targetBlank'] ? 'target="_blank"' : '' }}>
-        {{ $tree['infos']['name'] }}
-    </a>
+    @if (!empty($treeItem['infos']['html']))
+        {!! $treeItem['infos']['html'] !!}
+    @else
+        <a href="{{ $tree['infos']['href'] }}" class="{{ $tree['infos']['htmlClasses'] }}" {{ $tree['infos']['targetBlank'] ? 'target="_blank"' : '' }}>
+            {{ $tree['infos']['name'] }}
+        </a>
+    @endif
+
     @if (!empty($tree['children']))
         @include('laravel-nova-menu::front/partials/tree-item-list', ['tree' => $tree['children'], 'depth' => $depth])
     @endif
