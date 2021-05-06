4155de212c4ad1c44ef2467948fd28475755d862
Alexandre <ar.community@devnco.fr>
2021-05-06 11:52:50 +0200

    Compatibility with  cviebrock package (#20)
    * Compatibility with cviebrock package

--- src/Models/Menu.php
+++ src/Models/Menu.php
@@ -32,7 +32,7 @@ class Menu extends Model
      *
      * @return array
      */
-    public function sluggable()
+    public function sluggable(): array
     {
         return [
             'slug' => [
