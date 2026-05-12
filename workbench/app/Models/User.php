7145b0ad0bb3b40c09af04bd00d33b2605430489
Gilles Felix <felix@novius.fr>
2026-05-12 15:33:44 +0200

    cs-fix

--- workbench/app/Models/User.php
+++ workbench/app/Models/User.php
@@ -3,12 +3,12 @@
 namespace Workbench\App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Workbench\Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Workbench\Database\Factories\UserFactory;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
